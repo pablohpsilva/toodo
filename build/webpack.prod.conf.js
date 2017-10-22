f1160199722cc54d1d1d0bfa1e0fba62da14e300
git-directory-deploy <>
2017-10-21 22:29:44 -0200

    up to the top so far

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -2,6 +2,7 @@ var
   path = require('path'),
   config = require('../config'),
   cssUtils = require('./css-utils'),
+  glob = require('glob'),
   webpack = require('webpack'),
   merge = require('webpack-merge'),
   baseWebpackConfig = require('./webpack.base.conf'),
@@ -10,6 +11,11 @@ var
   SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin'),
   PreloadWebpackPlugin = require('preload-webpack-plugin'),
   OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin'),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  // PurifyCSSPlugin = require('purifycss-webpack'),
+  loadMinified = require('./load-minified'),
+  ImageminPlugin = require('imagemin-webpack-plugin').default,
+  StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin'),
   BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 // const publicUrl = publicPath.slice(0, -1);
@@ -28,12 +34,12 @@ module.exports = merge(baseWebpackConfig, {
       sourceMap: config.build.productionSourceMap,
       minimize: true,
       compress: {
-        warnings: true, // warn about potentially dangerous optimizations/code
+        warnings: false, // warn about potentially dangerous optimizations/code
         sequences: true,  // join consecutive statemets with the “comma operator”
         properties: true,  // optimize property access: a["foo"] → a.foo
         dead_code: true,  // discard unreachable code
         drop_debugger: true,  // discard “debugger” statements
-        unsafe: false, // some unsafe optimizations (see below)
+        unsafe: true, // some unsafe optimizations (see below)
         conditionals: true,  // optimize if-s and conditional expressions
         comparisons: true,  // optimize comparisons
         evaluate: true,  // evaluate constant expressions
@@ -60,12 +66,21 @@ module.exports = merge(baseWebpackConfig, {
     new ExtractTextPlugin({
       filename: '[name].[contenthash].css'
     }),
+    // new PurifyCSSPlugin({
+    //   // Give paths to parse for rules. These should be absolute!
+    //   paths: glob.sync(path.join(__dirname, 'build/*.html')),
+    //   purifyOptions: {
+    //     minify: true,
+    //     info: true,
+    //     whitelist: ['*q*', 'bg-grey-2', 'flex' , 'row', 'full-width', 'drag-wrapper', 'column', 'list-wrapper', 'drag-area', 'list-complete-item']
+    //   }
+    // }),
     new HtmlWebpackPlugin({
       filename: path.resolve(__dirname, '../dist/index.html'),
       template: 'src/index.html',
       inject: true,
       minify: {
-        tml5: true,
+        html5: true,
         useShortDoctype: true,
         decodeEntities: true,
         removeTagWhitespace: true,
@@ -86,26 +101,94 @@ module.exports = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
+      serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
+        './service-worker-prod.js'))}</script>`
+    }),
+    new StyleExtHtmlWebpackPlugin({
+      minify: true,
+      position: 'body-bottom'
+    }),
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../src/assets'),
+        to: config.build.publicPath + 'statics',
+        ignore: ['.*']
+      }
+    ]),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      maxConcurrency: 5,
+      optipng: 5,
+      gifsicle: 5,
+      jpegtran: {
+        progressive: true
+      },
+      svgo: {
+        cleanupAttrs: true,
+        removeDoctype: true,
+        removeXMLProcInst: true,
+        removeComments: true,
+        removeMetadata: true,
+        removeTitle: true,
+        removeDesc: true,
+        removeUselessDefs: true,
+        removeXMLNS: true,
+        removeEditorsNSData: true,
+        removeEmptyAttrs: true,
+        removeHiddenElems: true,
+        removeEmptyText: true,
+        removeEmptyContainers: true,
+        removeViewBox: true,
+        cleanupEnableBackground: true,
+        minifyStyles: true,
+        convertStyleToAttrs: true,
+        convertColors: true,
+        convertPathData: true,
+        convertTransform: true,
+        removeUnknownsAndDefaults: true,
+        removeNonInheritableGroupAttrs: true,
+        removeUselessStrokeAndFill: true,
+        removeUnusedNS: true,
+        cleanupIDs: true,
+        cleanupNumericValues: true,
+        cleanupListOfValues: true,
+        moveElemsAttrsToGroup: true,
+        moveGroupAttrsToElems: true,
+        collapseGroups: true,
+        removeRasterImages: true,
+        mergePaths: true,
+        convertShapeToPath: true,
+        sortAttrs: true,
+        transformsWithOnePath: true,
+        removeDimensions: true,
+        removeAttrs: true,
+        removeElementsByAttr: true,
+        addClassesToSVGElement: true,
+        addAttributesToSVGElement: true,
+        removeStyleElement: true,
+        removeScriptElement: true
+      }
     }),
     // split vendor js into its own file
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   async: true,
-    //   minChunks: function (module, count) {
-    //     // any required modules inside node_modules are extracted to vendor
-    //     return (
-    //       module.resource &&
-    //       /\.js$/.test(module.resource) &&
-    //       (
-    //         module.resource.indexOf('quasar') > -1 ||
-    //         module.resource.indexOf(
-    //           path.join(__dirname, '../node_modules')
-    //         ) === 0
-    //       )
-    //     )
-    //   }
-    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      async: true,
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          (
+            module.resource.indexOf('quasar') > -1 ||
+            module.resource.indexOf(
+              path.join(__dirname, '../node_modules')
+            ) === 0
+          )
+        )
+      }
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       async: true,
@@ -133,43 +216,30 @@ module.exports = merge(baseWebpackConfig, {
         )
       },
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'used-twice',
+      async: false,
+      children: false,
+      minChunks: function (module, count) {
+        return count >= 2;
+      },
+    }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor']
     }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'quasar',
-    //   async: true,
-    //   minChunks(module, count) {
-    //     var context = module.context;
-    //     var targets = ['quasar-framework', 'quasar-extras']
-    //     return context &&
-    //       context.indexOf('node_modules') >= 0 &&
-    //       targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
-    //   },
-    // }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'used-twice',
-      async: true,
-      children: false,
-      minChunks: function (module, count) {
-        return count >= 2;
-      },
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      include: ['manifest', 'vendor', 'quasar']
     }),
-    // Generate a service worker script that will precache, and keep up to date,
-    // the HTML & assets that are part of the Webpack build.
     new SWPrecacheWebpackPlugin({
       cacheId: 'toodo-app-v1',
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css,json,png,svg,woff}'],
       minify: true,
       stripPrefix: 'dist/'
-    }),
-    new PreloadWebpackPlugin({
-      rel: 'preload',
-      include: ['manifest', 'vendor', 'quasar']
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
